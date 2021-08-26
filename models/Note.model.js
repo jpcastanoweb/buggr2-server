9087d765f3d558a6f1fc73d53508631a19eaeb8f
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-26 18:20:54 -0500

    added timestamp to notes

--- models/Note.model.js
+++ models/Note.model.js
@@ -1,24 +1,29 @@
 const { Schema, model } = require("mongoose")
 
-const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Note's title is required"],
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Note's title is required"],
+    },
+    content: { type: String, default: "" },
+    belongsTo: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      // Instead of a hardcoded model name in `ref`, `refPath` means Mongoose
+      // will look at the `onModel` property to find the right model.
+      refPath: "onModel",
+    },
+    onModel: {
+      type: String,
+      required: true,
+      enum: ["Customer", "Opportunity", "Project"],
+    },
   },
-  content: { type: String, default: "" },
-  belongsTo: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    // Instead of a hardcoded model name in `ref`, `refPath` means Mongoose
-    // will look at the `onModel` property to find the right model.
-    refPath: "onModel",
-  },
-  onModel: {
-    type: String,
-    required: true,
-    enum: ["Customer", "Opportunity", "Project"],
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const Note = model("Note", noteSchema)
 
